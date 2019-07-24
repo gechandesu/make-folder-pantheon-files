6389ac386f6f5da46fbbc2c45b3ebe96c49f25d1
gechandesu <47027335+gechandesu@users.noreply.github.com>
2019-07-24 16:24:11 +0300

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,4 +6,4 @@ sudo chmod +x ~/.make-folder-with/make-folder-with
 
 mkdir ~/.local/share/contractor
 wget - P ~/.local/share/contractor https://raw.githubusercontent.com/gechandesu/make-folder-pantheon-files/master/make-folder-with.contract
-echo 'Exec=/home/'$USER'/.make-folder-with/make-folder-with %f' > ~/.local/share/contractor/make-folder-with.contract
+echo 'Exec=/home/'$USER'/.make-folder-with/make-folder-with %f' >> ~/.local/share/contractor/make-folder-with.contract
