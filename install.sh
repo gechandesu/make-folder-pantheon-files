024972f3763d2c0c919747db0e3514c63d24666b
gechandesu <47027335+gechandesu@users.noreply.github.com>
2019-07-24 14:40:32 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+mkdir ~/.make-folder-with
+wget -P ~/.make-folder-with https:// raw.githubusercontent.com/gechandesu/make-folder-pantheon-files/master/make-folder-with
+sudo chmod +x ~/.make-folder-with/make-folder-with
+
+mkdir ~/.local/share/contractor
+wget - P ~/.local/share/contractor https://raw.githubusercontent.com/gechandesu/make-folder-pantheon-files/master/make-folder-with.contract
+echo 'Exec=/home/'$USER'/.make-folder-with/make-folder-with %f' >> ~/.local/share/contractor/make-folder-with.contract
